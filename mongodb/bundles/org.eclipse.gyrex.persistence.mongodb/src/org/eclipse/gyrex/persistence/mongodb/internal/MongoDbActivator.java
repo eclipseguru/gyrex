b4149ffcc1d528b08309e0eee70891fd3f9a1f0c
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2012-01-12 11:58:06 +0100

    add debug options for MongoDB bundle
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>

--- mongodb/bundles/org.eclipse.gyrex.persistence.mongodb/src/org/eclipse/gyrex/persistence/mongodb/internal/MongoDbActivator.java
+++ mongodb/bundles/org.eclipse.gyrex.persistence.mongodb/src/org/eclipse/gyrex/persistence/mongodb/internal/MongoDbActivator.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2012 AGETO Service GmbH and others.
+ * All rights reserved.
+ *  
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Contributors:
+ *     Gunnar Wagenknecht - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.gyrex.persistence.mongodb.internal;
 
 import org.eclipse.gyrex.common.runtime.BaseBundleActivator;
@@ -45,6 +56,11 @@ public class MongoDbActivator extends BaseBundleActivator {
 
 		registry.stop();
 		registry = null;
+	}
+
+	@Override
+	protected Class getDebugOptions() {
+		return MongoDbDebug.class;
 	}
 
 	/**
